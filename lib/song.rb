23e8455c43dd4706393057214df7a1b19f036d04
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-09 03:17:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,13 +39,11 @@ class Song
     @@all.sort_by{ |song| song.name }
   end
   
-  
-  
-  
-  
-  
-  def self.new_from_filename
+  def self.new_from_filename(filename)
   end
+  
+  
+  
   
   def self.create_from_filename
   end
