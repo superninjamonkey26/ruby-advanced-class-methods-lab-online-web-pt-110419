d4ceebcbb7ef15f374ce8d8acb35b8a14b994397
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-09 03:22:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,8 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    split_filename = filename.slice(0, filename )
+   # split_filename = filename.slice(0, filename.length-4).split
+    split_filename = filename.chop.chop.chop.chop.split(" ")
   end
   
   
