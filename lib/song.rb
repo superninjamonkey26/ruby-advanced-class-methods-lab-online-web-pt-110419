220ed997183230d3181a496814410cd90a5495fa
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-09 03:27:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,7 @@ class Song
   end
   
   def self.new_from_filename(filename)
+    split_filename = filename.chop.chop.chop.chop.split(" - ")
     song = Song.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
