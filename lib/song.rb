29d68337c50234f100cb34ea970ab5630a3b6894
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-09 03:13:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,8 @@ class Song
   end
   
   def self.alphabetical
-    @@all.sort{ |a,b| a.name <=> b.name }
+#    @@all.sort{ |a,b| a.name <=> b.name }
+    @@all.sort_by{ |song| song.name }
   end
   
   
